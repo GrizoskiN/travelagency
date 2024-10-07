d03d00f7b7b8098d1367f50d60c1c4f393ac46a9
GrizoskiN <ngrizo@gmail.com>
2024-10-07 21:42:43 +0200

    new daypicker

--- app/components/Search/DestinationSelect.tsx
+++ app/components/Search/DestinationSelect.tsx
@@ -1,9 +1,28 @@
 import { useState } from "react";
-import { Select, SelectTrigger, SelectContent, SelectItem, SelectValue } from "@/components/ui/select";
-import { useDestinations } from "@/app/contexts/DestinationsContext";
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+} from "@/components/ui/select";
 
-const DestinationSelect: React.FC<{ onChange: (value: string) => void }> = ({ onChange }) => {
-  const { destinations } = useDestinations();
+
+type Destination = {
+  value: string;
+  label: string;
+  cities?: string[];
+};
+
+type DestinationSelectProps = {
+  destinations: Destination[];
+  onChange: (value: string) => void;
+};
+
+const DestinationSelect: React.FC<DestinationSelectProps> = ({
+  destinations,
+  onChange,
+}) => {
   const [selectedCountry, setSelectedCountry] = useState<string>("");
   const [availableCities, setAvailableCities] = useState<string[]>([]);
 
@@ -12,7 +31,9 @@ const DestinationSelect: React.FC<{ onChange: (value: string) => void }> = ({ on
     onChange(country);
 
     // Find the cities for the selected country and set them in state
-    const selectedDestination = destinations.find((destination) => destination.value === country);
+    const selectedDestination = destinations.find(
+      (destination) => destination.value === country
+    );
 
     if (selectedDestination?.cities) {
       setAvailableCities(selectedDestination.cities);
@@ -26,7 +47,7 @@ const DestinationSelect: React.FC<{ onChange: (value: string) => void }> = ({ on
   };
 
   return (
-    <div className="flex">
+    <div className="flex ">
       {/* Country Select */}
       <Select onValueChange={handleCountryChange}>
         <SelectTrigger className="rounded-full border-none shadow-none outline-none focus:ring-white">
@@ -43,7 +64,7 @@ const DestinationSelect: React.FC<{ onChange: (value: string) => void }> = ({ on
 
       {/* City Select: Only visible after a country is selected and there are cities available */}
       {selectedCountry && availableCities.length > 0 && (
-        <div className="ml-4">
+        <div className="">
           <Select onValueChange={handleCityChange}>
             <SelectTrigger className="rounded-full border-none shadow-none outline-none focus:ring-white">
               <SelectValue placeholder="Select a City" />
